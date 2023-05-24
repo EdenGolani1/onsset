88ee94831ad17184487d8964d33870575a3e7c1b
Eden Golani <golani.eden@gmail.com>
2023-05-24 18:22:49 +0300

    version 1.0 - cost function

---
+++ global_variables.py
@@ -0,0 +1,12 @@
+global outputFileName
+
+global endYear
+global projectLife
+global techLife
+techLife = 30  # of grid
+
+global settlementPower
+global settlementGridInvestment
+global settlementOperationAndMaintenance
+#global counter #todo _OUR: for counting iterations in get_lcoe
+global fuelCost
