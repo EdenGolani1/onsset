28d997ea4d1392db1aee148ee214e918b5d6fbd4
Eden Golani <golani.eden@gmail.com>
2023-05-31 21:41:19 +0300

    ver 1.1

--- global_variables.py
+++ global_variables.py
@@ -1,11 +1,11 @@
 global outputFileName
+global calibratedFileName
 
 global endYear
 global projectLife
 global techLife
 techLife = 30  # of grid
 
-global settlementPower
 global settlementGridInvestment
 global settlementOperationAndMaintenance
 #global counter #todo _OUR: for counting iterations in get_lcoe
